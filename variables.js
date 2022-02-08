949676ec1a1cfa20231b14b39ebb40c9ce981d82
Hannah Parker <hparke1402@gmail.com>
2022-02-07 22:37:00 -0500

    Assigning values to boolean variables

--- variables.js
+++ variables.js
@@ -5,3 +5,7 @@ var favoritePieceOfFurniture = "couch"
 var amountOfSyllables = 3
 var accountBalance = 19.14
 var amountOfSiblings = 4
+
+var isAwake = false
+var hasChildren = false
+var smellsGood = true
