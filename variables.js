4fc5ae1a9118ded0348219758a10c927c344c218
Hannah Parker <hparke1402@gmail.com>
2022-02-07 22:31:28 -0500

    Assigning values to number variables

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
 var mood = "sleepy"
 var animal = "cat"
-var favoritePieceOfFurniture= "couch"
+var favoritePieceOfFurniture = "couch"
+
+var amountOfSyllables = 3
+var accountBalance = 19.14
+var amountOfSiblings = 4
